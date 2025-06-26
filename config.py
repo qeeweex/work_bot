cceb67d039dbee089368f83f232ffad669ebe5bd
unknown <garmashkir09@mail.ru>
2025-06-26 23:20:28 +0300

    вынес такие данные как: ключ, название базы данных, и id администратора в отдельный файл

---
+++ config.py
@@ -0,0 +1,3 @@
+API_TOKEN = "7985036267:AAHl2fN-aN216zwgmCspvo1s2GOypZ-U-1k"  # Вставьте сюда токен вашего Telegram-бота
+DB_NAME = "database.db"        # Имя файла базы данных
+ADMINS = [1]           # Список Telegram ID администраторов (можно добавить свои)
